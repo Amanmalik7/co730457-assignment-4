4bf93d6795c351d0ce118233e80ec2c44f7da675
visualstudio <visualstudio@122-PC24>
2019-04-10 10:44:03 -0400

    SECTION-1

--- co730457-assignment-4/Program.cs
+++ co730457-assignment-4/Program.cs
@@ -15,6 +15,8 @@ namespace co730457_assignment_4
         {
             Program p = new Program();
             p.Beowulf = new ArrayList();
+            p.ReadTextFiles();
+            Console.ReadLine();
 
         }
         public void Run() { this.ReadTextFiles(); }
@@ -32,7 +34,8 @@ namespace co730457_assignment_4
                     Beowulf.Add(ln);
                 }
                 file.Close();
-                Console.WriteLine($"File has {counter} lines.");
+                counter = File.ReadLines("").Count();
+                Console.WriteLine("total number of lines are"+counter);
             }
         }
         public int FindNumberOfBlankSpaces(string line)
